73a9f98b3c707fcc9e3fbac5669efa60ac018e73
Tanner Smith <tanner@tssoftware.net>
2011-05-30 21:23:25 -0400

    Remove all RoR stuff.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FoodNoms::Application.config.secret_token = '38829b44c03c8f85241fb3d6d14baffaecace557b1541bc5e3c3e543a52d3e0313bcebaeb6bbece1aa9eae4d122e6528d6cf7504bfcf615b8f641a506dfc61e1'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-FoodNoms::Application.config.session_store :cookie_store, :key => '_Food-Noms_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# FoodNoms::Application.config.session_store :active_record_store
