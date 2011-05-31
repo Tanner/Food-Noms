f4ac1830de3aac73f87571081f5445e14133c300
Tanner Smith <tanner@tssoftware.net>
2011-05-31 17:36:15 -0400

    Create two basic models - Restaurant and Nom.

--- food_noms/noms/models.py
+++ food_noms/noms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+     name = models.CharField(max_length=150)
+
+     def __unicode__(self):
+          return self.name
+     def __str__(self):
+          return __str__()
+
+class Nom(models.Model):
+     restaurant = models.ForeignKey(Restaurant)
+     name = models.CharField(max_length=150)
+     
+     def __unicode__(self):
+          return self.name + " at " + unicode(self.restaurant)
