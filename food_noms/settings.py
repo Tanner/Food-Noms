3c07830395287168f4eb7ccb3e343029e40ef6f7
Tanner Smith <tanner@tssoftware.net>
2011-06-01 15:06:57 -0400

    Add a template directory in the root directory. Magic relativity.

--- food_noms/settings.py
+++ food_noms/settings.py
@@ -1,5 +1,9 @@
 # Django settings for food_noms project.
 
+# Get the pagh of where we are so we don't have to have exact paths
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -106,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
