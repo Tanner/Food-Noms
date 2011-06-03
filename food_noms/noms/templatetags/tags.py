5b564fddad7ac7bede13b3e60c5a34ec39c6807f
Tanner Smith <tanner@tssoftware.net>
2011-06-02 20:48:49 -0400

    Use an improved function for the navigation. It doesn't do stupid stuff anymore.
    http://stackoverflow.com/questions/340888/navigation-in-django

--- food_noms/noms/templatetags/tags.py
+++ food_noms/noms/templatetags/tags.py
@@ -2,10 +2,23 @@ from django import template
 
 register = template.Library()
 
-@register.simple_tag
-def active(request, pattern):
+@register.tag
+def active(parser, token):
     import re
-    if re.search(pattern, request.path):
-        return 'active'
-    return ''
+    args = token.split_contents()
+    template_tag = args[0]
+    if len(args) < 2:
+        raise template.TemplateSyntaxError, "%r tag requires at least one argument" % template_tag
+    return NavSelectedNode(args[1:])
+
+class NavSelectedNode(template.Node):
+    def __init__(self, patterns):
+        self.patterns = patterns
+    def render(self, context):
+        path = context['request'].path
+        for p in self.patterns:
+            pValue = template.Variable(p).resolve(context)
+            if path == pValue:
+                return "active"
+        return ""
 
