1c5388e506b270d8fbd6c7493be6eff8081289b0
Tanner Smith <tanner@tssoftware.net>
2011-06-10 12:00:04 -0400

    Modify the models for ratings for our purposes.

--- food_noms/ratings/models.py
+++ food_noms/ratings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from noms import models as nomModels
 
-# Create your models here.
+class Rating(models.Model):
+     nom = models.ForeignKey(nomModels.Nom)
+
+class Question(models.Model):
+     rating = models.ForeignKey(Rating)
+     question = models.CharField(max_length=200)
+     rate = models.IntegerField()
+     hasFreeResponse = models.BooleanField()
+     freeResponse = models.CharField(max_length=1000)
