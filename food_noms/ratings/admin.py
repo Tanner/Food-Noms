de82a1252c327d8b195632328b53f2525ecb7c96
Tanner Smith <tanner@tssoftware.net>
2011-06-11 19:57:38 -0400

    Customize the admin section so that we can add responses when adding/editing a rating.

--- food_noms/ratings/admin.py
+++ food_noms/ratings/admin.py
@@ -1,6 +1,13 @@
 from ratings.models import Rating, Question, Response
 from django.contrib import admin
 
-admin.site.register(Rating)
+class ResponseInline(admin.TabularInline):
+     model = Response
+     extra = 1
+
+class RatingAdmin(admin.ModelAdmin):
+     inlines = [ResponseInline]
+
+admin.site.register(Rating, RatingAdmin)
 admin.site.register(Question)
 admin.site.register(Response)
