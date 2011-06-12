210f58c880d98646f61bc3eb7f29bee0ff05de9c
Tanner Smith <tanner@tssoftware.net>
2011-06-11 23:41:52 -0400

    Show list of ratings at each nom page.

--- food_noms/noms/views.py
+++ food_noms/noms/views.py
@@ -26,8 +26,10 @@ def restaurantDetail(request, restaurant_id):
 def nomDetail(request, restaurant_id, nom_id):
      try:
           n = Nom.objects.get(pk=nom_id)
+          ratings = n.rating_set.all()
+
           t = loader.get_template("base_nom.html")
-          c = RequestContext(request, {"nom": n})
+          c = RequestContext(request, {"nom": n, "ratings": ratings})
           return HttpResponse(t.render(c))
      except Nom.DoesNotExist:
           # Should we redirect to the restaurant page instead of 404?
