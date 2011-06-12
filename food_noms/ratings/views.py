fad748496aa21839d9503e6e77da3327e2caf004
Tanner Smith <tanner@tssoftware.net>
2011-06-11 21:43:50 -0400

    Add a nom_id check for rating ad and fix rating detail page exception catching.

--- food_noms/ratings/views.py
+++ food_noms/ratings/views.py
@@ -12,35 +12,38 @@ def detail(request, rating_id):
           t = loader.get_template("base_detail.html")
           c = RequestContext(request, {"rating": rating, "responses": responses})
           return HttpResponse(t.render(c))
-     except Nom.DoesNotExist:
+     except Rating.DoesNotExist:
           raise Http404
 
 def add(request, nom_id):
-     nom = Nom.objects.get(pk=nom_id)
-     questions = Question.objects.all()
+     try:
+          nom = Nom.objects.get(pk=nom_id)
+          questions = Question.objects.all()
 
-     if request.method == "POST":
-          form = AddForm(questions, request.POST)
-          if form.is_valid():
-               data = form.cleaned_data
+          if request.method == "POST":
+               form = AddForm(questions, request.POST)
+               if form.is_valid():
+                    data = form.cleaned_data
 
-               rating = Rating.objects.create(nom=Nom.objects.get(pk=nom_id))
-               for question in questions:
-                    id = question.id
-                    if question.hasRate:
-                         Response.objects.create(rating=rating, question=question, rate=data[str(id)], freeResponse="")
-                    if question.hasFreeResponse:
-                         Response.objects.create(rating=rating, question=question, rate=0, freeResponse=data[str(id)])
+                    rating = Rating.objects.create(nom=Nom.objects.get(pk=nom_id))
+                    for question in questions:
+                         id = question.id
+                         if question.hasRate:
+                              Response.objects.create(rating=rating, question=question, rate=data[str(id)], freeResponse="")
+                         if question.hasFreeResponse:
+                              Response.objects.create(rating=rating, question=question, rate=0, freeResponse=data[str(id)])
 
-               t = loader.get_template("base_added.html")
-               c = RequestContext(request, {"nom": nom})
-               return HttpResponse(t.render(c))
-     else:
-          form = AddForm(questions)
-     
-     t = loader.get_template("base_add.html")
-     c = RequestContext(request, {"form": form, "nom": nom})
-     return HttpResponse(t.render(c))
+                    t = loader.get_template("base_added.html")
+                    c = RequestContext(request, {"nom": nom})
+                    return HttpResponse(t.render(c))
+          else:
+               form = AddForm(questions)
+          
+          t = loader.get_template("base_add.html")
+          c = RequestContext(request, {"form": form, "nom": nom})
+          return HttpResponse(t.render(c))
+     except Nom.DoesNotExist:
+          raise Http404
 
 def delete(request, rating_id):
      try:
