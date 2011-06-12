7d26c7e117a40a3f82fef8f2d1a7e42ac85102be
Tanner Smith <tanner@tssoftware.net>
2011-06-11 20:07:17 -0400

    Add ability in admin to add noms while modifying a restaurant.

--- food_noms/noms/admin.py
+++ food_noms/noms/admin.py
@@ -1,5 +1,12 @@
 from noms.models import Restaurant, Nom
 from django.contrib import admin
 
-admin.site.register(Restaurant)
+class NomInline(admin.TabularInline):
+     model = Nom
+     extra = 1
+
+class RestaurantAdmin(admin.ModelAdmin):
+     inlines = [NomInline]
+
+admin.site.register(Restaurant, RestaurantAdmin)
 admin.site.register(Nom)
