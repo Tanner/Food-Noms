283d85008282fec23e019d6ff407c376b4a1775e
Tanner Smith <tanner@tssoftware.net>
2011-06-28 09:03:37 -0400

    Delete old migrations and add a new initial one.
    Converting the old ones would be too hard to rewrite all the schema migrations.

--- food_noms/reviews/migrations/0001_initial.py
+++ food_noms/reviews/migrations/0001_initial.py
@@ -8,46 +8,93 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         
-        # Adding model 'Rating'
-        db.create_table('ratings_rating', (
+        # Adding model 'Review'
+        db.create_table('reviews_review', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('nom', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['noms.Nom'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True)),
         ))
-        db.send_create_signal('ratings', ['Rating'])
+        db.send_create_signal('reviews', ['Review'])
 
         # Adding model 'Question'
-        db.create_table('ratings_question', (
+        db.create_table('reviews_question', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('question', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('hasRate', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('hasFreeResponse', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('type', self.gf('django.db.models.fields.CharField')(max_length=200)),
         ))
-        db.send_create_signal('ratings', ['Question'])
+        db.send_create_signal('reviews', ['Question'])
 
-        # Adding model 'Response'
-        db.create_table('ratings_response', (
+        # Adding model 'FreeResponse'
+        db.create_table('reviews_freeresponse', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('rating', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['reviews.Rating'])),
+            ('review', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['reviews.Review'])),
             ('question', self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', to=orm['reviews.Question'])),
-            ('rate', self.gf('django.db.models.fields.IntegerField')()),
-            ('freeResponse', self.gf('django.db.models.fields.CharField')(max_length=1000)),
+            ('freeResponse', self.gf('django.db.models.fields.CharField')(max_length=1000, null=True, blank=True)),
         ))
-        db.send_create_signal('ratings', ['Response'])
+        db.send_create_signal('reviews', ['FreeResponse'])
+
+        # Adding model 'RatingResponse'
+        db.create_table('reviews_ratingresponse', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('review', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['reviews.Review'])),
+            ('question', self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', to=orm['reviews.Question'])),
+            ('rate', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('reviews', ['RatingResponse'])
 
 
     def backwards(self, orm):
         
-        # Deleting model 'Rating'
-        db.delete_table('ratings_rating')
+        # Deleting model 'Review'
+        db.delete_table('reviews_review')
 
         # Deleting model 'Question'
-        db.delete_table('ratings_question')
+        db.delete_table('reviews_question')
 
-        # Deleting model 'Response'
-        db.delete_table('ratings_response')
+        # Deleting model 'FreeResponse'
+        db.delete_table('reviews_freeresponse')
+
+        # Deleting model 'RatingResponse'
+        db.delete_table('reviews_ratingresponse')
 
 
     models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         'noms.nom': {
             'Meta': {'object_name': 'Nom'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -59,26 +106,32 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '150'})
         },
-        'reviews.question': {
-            'Meta': {'object_name': 'Question'},
-            'hasFreeResponse': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'hasRate': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'question': ('django.db.models.fields.CharField', [], {'max_length': '200'})
-        },
-        'reviews.rating': {
-            'Meta': {'object_name': 'Rating'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'nom': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['noms.Nom']"})
-        },
-        'reviews.response': {
-            'Meta': {'object_name': 'Response'},
-            'freeResponse': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+        'reviews.freeresponse': {
+            'Meta': {'object_name': 'FreeResponse'},
+            'freeResponse': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'question': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['reviews.Question']"}),
-            'rate': ('django.db.models.fields.IntegerField', [], {}),
-            'rating': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reviews.Rating']"})
+            'review': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reviews.Review']"})
+        },
+        'reviews.question': {
+            'Meta': {'object_name': 'Question'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'reviews.ratingresponse': {
+            'Meta': {'object_name': 'RatingResponse'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['reviews.Question']"}),
+            'rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'review': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reviews.Review']"})
+        },
+        'reviews.review': {
+            'Meta': {'object_name': 'Review'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nom': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['noms.Nom']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'})
         }
     }
 
-    complete_apps = ['ratings']
+    complete_apps = ['reviews']
